eff457c46f3a6bcdaaea494f18799be84fa4f059
Magus <mfail792@gmail.com>
2019-10-02 20:55:24 -0500

    updated basic routes for home.html and survey page

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,14 +1,26 @@
 //Routes
+var express = require('express');
+var path = require('path');
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Basic routes to home.html and survey.html
+app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+  });
+  
+  
+  
 
 
-app.get("/home", function (req, res) {
-    res.send("Welcome to FriendFinder!");
-});
 
-
-
-app.get("/survey", function (req, res) {
-    res.send("Survey Page!")
-});
 
 
--- server.js
+++ server.js
@@ -4,4 +4,7 @@ var path = require('path');
 var app = express();
 var PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
