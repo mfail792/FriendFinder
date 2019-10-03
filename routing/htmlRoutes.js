a1f62f29338993b7ec22ecbea86e693c86a73cab
Magus <mfail792@gmail.com>
2019-10-02 20:58:33 -0500

    updated catch all route with * path

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -9,11 +9,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Basic routes to home.html and survey.html
-app.get("/home", function(req, res) {
+app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
   });
 
-  app.get("/survey", function(req, res) {
+  app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "survey.html"));
   });
   
