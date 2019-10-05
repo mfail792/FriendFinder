1971837ce730d36233fd980d446886beb1ff09e2
Magus <mfail792@gmail.com>
2019-10-05 00:16:15 -0500

    moved button click event to apiRoutes, posting JSON data to api/friends

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,21 +1,34 @@
+// Displays all friends
 app.get("/api/friends", function (req, res) {
-    return res.json(addFriend);
+    return res.json(characters);
 });
 
-app.post("/api/friends", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var newFriend = req.body;
+//Grabs user input data and scores, posts to friendsCache array in friends.js-->
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+    var newFriend = {
+        name: $("#name").val(),
+        photo: $("#photo").val(),
+        scores: [
+            $("#q1").val(),
+            $("#q2").val(),
+            $("#q3").val(),
+            $("#q4").val(),
+            $("#q5").val(),
+            $("#q6").val(),
+            $("#q7").val(),
+            $("#q8").val(),
+            $("#q9").val(),
+            $("#q10").val()
+        ]
+    };
 
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-
-    console.log(newFriend);
-
-    characters.push(newFriend);
-
-    res.json(newFriend);
+    //sends to friends.js for storage
+    $.post("/api/friends", newFriend)
+        .then(function (data) {
+            console.log("survey.html", data);
+            alert("Adding friend...");
+        });
 });
 
 
