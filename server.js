d27c71c8938397ba78770f6f3b7c69a24837e9fd
Magus <mfail792@gmail.com>
2019-10-05 22:35:57 -0500

    test2

--- server.js
+++ server.js
@@ -7,14 +7,15 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
 
-// app.get("/", function (req, res) {
-//     res.sendFile(path.join(__dirname, "./home.html"));
-// });
+
 
 // app.get("/survey", function (req, res) {
 //     res.sendFile(path.join(__dirname, "./survey.html"));
