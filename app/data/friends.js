29ca36852bdb22689658675ecb83ceeb6ef38eb9
Magus <mfail792@gmail.com>
2019-10-06 00:26:50 -0500

    restructured files/directories

---
+++ app/data/friends.js
@@ -0,0 +1,97 @@
+// Friend Finder (DATA)
+// =============================================================
+var friendCache = [
+  {
+    "name": "Bort",
+    "photo": "https://404store.com/2017/12/08/Er-weird-O-o-random-23398022-600-678.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    "name": "Zort",
+    "photo": "https://pbs.twimg.com/profile_images/653700295395016708/WjGTnKGQ_400x400.png",
+    "scores": [
+      3,
+      4,
+      2,
+      1,
+      5,
+      1,
+      3,
+      5,
+      2,
+      4
+    ]
+  },
+  {
+    "name": "Mort",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS8CNZE_Iy3SuGmF03ktybvYfYBG56TITeyu4w0v4v27OK2cE13",
+    "scores": [
+      2,
+      5,
+      3,
+      2,
+      4,
+      3,
+      5,
+      2,
+      3,
+      2
+    ]
+  },
+  {
+    "name": "Gort",
+    "photo": "https://nintendosoup.com/wp-content/uploads/2019/09/Sir-fetch-memes-1038x576.jpg",
+    "scores": [
+      2,
+      4,
+      2,
+      1,
+      1,
+      5,
+      4,
+      5,
+      3,
+      1
+    ]
+  },
+  {
+    "name": "Dort",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRN0TyKB-bciZuiyX5ZzJyKGhLSjAPh-ZrUkhr6WJXJzY0qkteE",
+    "scores": [
+      4,
+      3,
+      5,
+      4,
+      4,
+      1,
+      1,
+      3,
+      4,
+      5
+    ]
+  }
+]
+
+
+module.exports = friendCache;
+
+
+//test
+
+
+
+
+
+
+
