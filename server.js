7d85cd158ebb347b2bfbd3a3a7885a7e89be5c88
Magus <mfail792@gmail.com>
2019-10-05 22:53:49 -0500

    test7

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, './app/public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 app.get('/', function (req, res) {
     res.render('home.html');
 });
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
